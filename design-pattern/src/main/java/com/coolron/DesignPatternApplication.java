1ae5312de146b5c1d3645719f9dd99c1b014f251
hd_xf <xiongfeng@hdkjrd.com>
2020-11-21 10:29:15 +0800

    init

---
+++ design-pattern/src/main/java/com/coolron/DesignPatternApplication.java
@@ -0,0 +1,11 @@
+package com.coolron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternApplication.class);
+    }
+}
